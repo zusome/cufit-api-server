7f76a13179866ad62bba8bfd00b2ce78a61783a0
unanchoi <unanc2@gmail.com>
2024-11-27 23:04:18 +0900

    폴더 구조 잡기

--- cufit-api/build.gradle.kts
+++ cufit-api/build.gradle.kts
@@ -25,6 +25,12 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// JPA
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 }
 
 kotlin {
