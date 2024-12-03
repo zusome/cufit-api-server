441b8b9c9712816cbf37cb2c5efc9cc8d18cc31c
unanchoi <unanc2@gmail.com>
2024-12-03 19:54:29 +0900

    style: 알림 주석 수정

--- cufit-api/src/main/kotlin/com/official/cufitapi/domain/application/NotificationMessageGenerator.kt
+++ cufit-api/src/main/kotlin/com/official/cufitapi/domain/application/NotificationMessageGenerator.kt
@@ -1,5 +1,19 @@
 package com.official.cufitapi.domain.application
 
+/*
+    <프로필 멘트>
+    - OOO님 등록 완료 OOO님이 프로필을 완성했어요
+    <연결 요청 멘트>
+    - OOO님에게 하트 보내기 완료\nXXX님의 카드를 보냈어요
+    <case.1 실패 멘트 (12시간 지나도 누르지 않음)>
+    - OOO님 연결되지 않았어요.\n12시간이 지나 연결 실패했어요.
+    <case.2 실패 멘트 (상대후보자가 거절함)>
+    - OOO님 연결되지 않았어요.\nXXX님이 하트를 거절했어요.
+    <case.2 실패 멘트 (내가 거절함)>
+    - OOO님 연결되지 않았어요.\n OOO님이 XXX님의 하트를 거절했어요.
+    <case.3 실패 멘트 (둘다 거절함)>
+    - OOO님 연결되지 않았어요.\n 후보자 모두 하트를 거절했어요.
+ */
 class NotificationMessageGenerator {
     companion object {
         @JvmStatic
@@ -27,19 +41,3 @@ class NotificationMessageGenerator {
 
     }
 }
-
-//    프로필 멘트
-//    OOO님 등록 완료 OOO님이 프로필을 완성했어요.
-
-//    연결 요청 멘트
-//    OOO님에게 하트 보내기 완료 XXX님의 카드를 보냈어요.
-
-//    case.1 실패 멘트 (12시간 지나도 누르지 않음)
-//    OOO님 연결되지 않았어요. 12시간이 지나 연결 실패했어요.
-//    case.2 실패 멘트 (상대후보자가 거절함)
-//    OOO님 연결되지 않았어요. XXX님이 하트를 거절했어요.
-//    case.2 실패 멘트 (내가 거절함)
-//    OOO님 연결되지 않았어요. OOO님이 XXX님의 하트를 거절했어요.
-//    case.3 실패 멘트 (둘다 거절함)
-//    OOO님 연결되지 않았어요. 후보자 모두 하트를 거절했어요.
-//}
