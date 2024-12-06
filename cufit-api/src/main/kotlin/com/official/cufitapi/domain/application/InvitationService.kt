6e6f19ec81bd403e56bb19857429064c155a735b
unanchoi <unanc2@gmail.com>
2024-12-06 22:25:41 +0900

    docs: 주선자, 후보자 구분할 수 있는 초대코드 생성

--- cufit-api/src/main/kotlin/com/official/cufitapi/domain/application/InvitationService.kt
+++ cufit-api/src/main/kotlin/com/official/cufitapi/domain/application/InvitationService.kt
@@ -2,6 +2,7 @@ package com.official.cufitapi.domain.application
 
 import com.official.cufitapi.common.exception.InvalidRequestException
 import com.official.cufitapi.domain.api.dto.InvitationCodeRequest
+import com.official.cufitapi.domain.api.dto.invitation.InvitationCodeResponse
 import com.official.cufitapi.domain.infrastructure.entity.Invitation
 import com.official.cufitapi.domain.infrastructure.repository.InvitationJpaRepository
 import com.official.cufitapi.domain.infrastructure.repository.MemberJpaRepository
@@ -27,14 +28,17 @@ class InvitationService(
         }
     }
 
-    fun generateInvitationCode(memberId: Long) : InvitationGenerateResponse {
+    // TODO : 주선자랑 후보자 구분할 수 있는 초대코드 생성
+    fun generateInvitationCode(memberId: Long) : InvitationCodeResponse {
         val member = memberJpaRepository.findByIdOrNull(memberId) ?: throw InvalidRequestException("잘못된 사용자 ID 요청 : ${memberId}")
         val invitationCode = generateRandomBase62String()
-        val invitation = Invitation(
+        val invitation = invitationJpaRepository.save(Invitation(
             code = invitationCode,
             memberId = member.id!!
+        ))
+        return InvitationCodeResponse(
+            invitationCode = invitation.code
         )
-        invitationJpaRepository.save(invitation)
     }
 
     private fun generateRandomBase62String(length: Int = 8): String {
