58937a272f07bba7d1c6d7ad01011d2295a61db9
unanchoi <unanc2@gmail.com>
2024-12-13 00:41:16 +0900

    초대코드 생성 요청 swagger 추가

--- src/main/kotlin/com/official/cufitapi/domain/api/dto/invitation/InvitationCodeGenerateRequest.kt
+++ src/main/kotlin/com/official/cufitapi/domain/api/dto/invitation/InvitationCodeGenerateRequest.kt
@@ -2,8 +2,12 @@ package com.official.cufitapi.domain.api.dto.invitation
 
 import com.official.cufitapi.domain.enums.MatchMakerCandidateRelationType
 import com.official.cufitapi.domain.enums.MemberType
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(name="초대코드 생성 요청")
 data class InvitationCodeGenerateRequest(
+    @Schema(description = "후보자, 주선자", example = "MATCHMAKER/CANDIDATE")
     val memberType: MemberType,
+    @Schema(description = "후보자, 주선자의 관계", example = "FRIEND")
     val relationType: MatchMakerCandidateRelationType
 )
