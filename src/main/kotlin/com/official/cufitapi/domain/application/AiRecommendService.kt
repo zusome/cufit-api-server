279a24499a1c9b986494822d4150bacab4851759
unanchoi <unanc2@gmail.com>
2024-12-19 19:59:19 +0900

    fix: AI Recommend service 정의

---
+++ src/main/kotlin/com/official/cufitapi/domain/application/AiRecommendService.kt
@@ -0,0 +1,18 @@
+package com.official.cufitapi.domain.application
+
+import org.springframework.stereotype.Service
+
+@Service
+class AiRecommendService {
+
+    // TODO: AI 서버로부터 추천 결과 받아오기
+    fun getMatchingResult() {
+
+    }
+
+    // TODO : AI 서버에 유저 정보 전송
+    fun sendCandidateInfo() {
+
+    }
+
+}
