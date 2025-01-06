6ea367ea795083968dbc6448de43b38d59112a3c
woozi <kwj1270@naver.com>
2025-01-07 02:44:06 +0900

    feat: 사용자 타입 조회 API 구현

--- src/main/kotlin/com/official/cufitapi/domain/api/MemberApi.kt
+++ src/main/kotlin/com/official/cufitapi/domain/api/MemberApi.kt
@@ -2,13 +2,14 @@ package com.official.cufitapi.domain.api
 
 import com.official.cufitapi.common.annotation.Authorization
 import com.official.cufitapi.common.annotation.AuthorizationType
+import com.official.cufitapi.common.annotation.AuthorizationUser
 import com.official.cufitapi.common.api.HttpResponse
 import com.official.cufitapi.domain.api.docs.MemberApiDocs
 import com.official.cufitapi.domain.api.dto.MemberInfoResponse
 import com.official.cufitapi.domain.api.dto.MemberProfileRequest
+import com.official.cufitapi.domain.api.dto.MemberTypeInfoResponse
 import com.official.cufitapi.domain.application.MemberService
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,11 +19,23 @@ class MemberApi(
     private val memberService: MemberService
 ) : MemberApiDocs {
 
+    // 유저 타입 조회
+    @GetMapping("/members/type")
+    override fun getMemberTypeInfo(
+        @Authorization(AuthorizationType.ALL) authorizationUser: AuthorizationUser
+    ): HttpResponse<MemberTypeInfoResponse> {
+        val member = memberService.findById(authorizationUser.userId)
+        return HttpResponse.of(
+            HttpStatus.NO_CONTENT,
+            MemberTypeInfoResponse(member.name.isBlank(), member.memberType.name)
+        )
+    }
+
     // 유저 정보 조회
     @GetMapping("/members")
     fun getMemberInfo(
         @Authorization(AuthorizationType.ALL) memberId: Long
-    ) : HttpResponse<MemberInfoResponse> {
+    ): HttpResponse<MemberInfoResponse> {
         val memberInfo = memberService.getMemberInfo(memberId)
         return HttpResponse.of(HttpStatus.NO_CONTENT, memberInfo)
     }
@@ -32,9 +45,8 @@ class MemberApi(
     fun updateProfile(
         @Authorization(AuthorizationType.ALL) memberId: Long,
         @RequestBody request: MemberProfileRequest
-    ) : HttpResponse<Unit> {
+    ): HttpResponse<Unit> {
         memberService.updateMemberProfile(memberId, request)
         return HttpResponse.of(HttpStatus.OK, Unit)
     }
-
 }
---
+++ src/main/kotlin/com/official/cufitapi/domain/api/dto/MemberTypeInfoResponse.kt
@@ -0,0 +1,6 @@
+package com.official.cufitapi.domain.api.dto
+
+data class MemberTypeInfoResponse(
+    val isProfileCompleted: Boolean,
+    val memberType: String
+)
