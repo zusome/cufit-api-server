c8fb5a029521e395a1ac875fb76a00b11abb27ea
unanchoi <unanc2@gmail.com>
2025-01-27 03:11:23 +0900

    chore: HealthCheckApi naming 변경

--- src/main/kotlin/com/official/cufitapi/HealthCheckApi.kt
+++ src/main/kotlin/com/official/cufitapi/HealthCheckApi.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class HealthCheckController {
+class HealthCheckApi {
 
     @GetMapping("/health")
     fun healthCheck() : String {
