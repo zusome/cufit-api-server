bfd611a7bea8ff522c709fe089e6fcf7c32d7abe
woozi <kwj1270@naver.com>
2025-02-08 14:07:33 +0900

    feature: MatchMakerDao 수정

--- src/main/kotlin/com/official/cufitapi/domain/member/infrastructure/persistence/dao/MatchMakerDao.kt
+++ src/main/kotlin/com/official/cufitapi/domain/member/infrastructure/persistence/dao/MatchMakerDao.kt
@@ -1,5 +1,6 @@
 package com.official.cufitapi.domain.member.infrastructure.persistence.dao
 
+import com.official.cufitapi.domain.member.api.dto.candidate.CandidateImage
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Component
 import javax.sql.DataSource
@@ -10,85 +11,13 @@ class MatchMakerDao(
 ) {
     private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate = NamedParameterJdbcTemplate(dataSource)
 
-    fun findAllCandidates(memberId: Long): MatchCandidates {
-        val sql = """
-        SELECT c.name, r.relation, a.dateName, a.arrangementStatus, c.hasProfile, c.isMatchingPaused
-        FROM MatchCandidateEntity c
-        LEFT JOIN MatchRelationEntity r ON c.id = r.candidateId
-        LEFT JOIN MatchArrangementEntity a ON c.id = a.candidateId
-        WHERE r.memberId = :memberId
-    """
-        val params = mapOf("memberId" to memberId)
-        val candidates = namedParameterJdbcTemplate.query(sql, params) { rs, _ ->
-            MatchCandidate(
-                name = rs.getString("name"),
-                relation = rs.getString("relation"),
-                arrangements = listOf(
-                    ArrangementInfo(
-                        dateName = rs.getString("dateName"),
-                        arrangementStatus = rs.getString("arrangementStatus")
-                    )
-                ),
-                hasProfile = rs.getBoolean("hasProfile"),
-                isMatchingPaused = rs.getBoolean("isMatchingPaused")
-            )
-        }
-        return MatchCandidates(candidates)
-    }
-
-    fun findAllOtherCandidates(matchMakerId: Long): List<OtherMatchCandidate> {
-        val sql = """
-        SELECT c.* FROM MatchCandidateEntity c
-        LEFT JOIN MatchRelationEntity r ON c.id = r.candidateId
-        WHERE r.matchMakerId IS NULL OR r.matchMakerId != :matchMakerId
-    """
-        val params = mapOf("matchMakerId" to matchMakerId)
-        return namedParameterJdbcTemplate.query(sql, params) { rs, _ ->
-            OtherMatchCandidate(
-                id = rs.getLong("id"),
-                name = rs.getString("name"),
-                yearOfBirth = rs.getInt("yearOfBirth"),
-                mbti = rs.getString("mbti"),
-                height = rs.getInt("height"),
-                station = rs.getString("station"),
-                job = rs.getString("job"),
-                degrees = rs.getInt("degrees"),
-                matchMakerRelation = rs.getString("matchMakerRelation"),
-                matchMakerName = rs.getString("matchMakerName"),
-                idealHeightRange = rs.getString("idealHeightRange").split(",").map { it.toInt() },
-                idealAgeRange = rs.getString("idealAgeRange").split(","),
-                idealMbti = rs.getString("idealMbti").split(",")
-            )
-        }
-    }
-
-    fun countCandidatesByMemberId(memberId: Long): Long {
-        val sql = """
-        SELECT COUNT(*)
-        FROM MatchCandidateEntity c
-        LEFT JOIN MatchRelationEntity r ON c.id = r.candidateId
-        WHERE r.memberId = :memberId
-    """
-        val params = mapOf("memberId" to memberId)
-        return namedParameterJdbcTemplate.queryForObject(sql, params, Long::class.java) ?: 0L
-    }
-
-    fun countOtherCandidatesByMemberId(memberId: Long): Long {
-        val sql = """
-        SELECT COUNT(*)
-        FROM MatchCandidateEntity c
-        LEFT JOIN MatchRelationEntity r ON c.id = r.candidateId
-        WHERE r.memberId IS NULL OR r.memberId != :memberId
-    """
-        val params = mapOf("memberId" to memberId)
-        return namedParameterJdbcTemplate.queryForObject(sql, params, Long::class.java) ?: 0L
-    }
 
     data class MatchCandidates(
         val candidates: List<MatchCandidate>,
     )
 
     data class MatchCandidate(
+        val image: String,
         val name: String,
         val relation: String,
         val arrangements: List<ArrangementInfo>,
@@ -97,6 +26,7 @@ class MatchMakerDao(
     )
 
     data class ArrangementInfo(
+        val profileImage: String,
         val dateName: String,
         val arrangementStatus: String,
     )
@@ -107,6 +37,7 @@ class MatchMakerDao(
 
     data class OtherMatchCandidate(
         val id: Long,
+        val images: List<CandidateImage>,
         val name: String,
         val yearOfBirth: Int,
         val mbti: String,
