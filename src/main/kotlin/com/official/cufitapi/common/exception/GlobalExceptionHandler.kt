7ae629bc517393945e0bba07a7b902aee196fd3a
unanchoi <unanc2@gmail.com>
2025-04-20 16:18:47 +0900

    feat: Sentry capture 체크

--- src/main/kotlin/com/official/cufitapi/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/official/cufitapi/common/exception/GlobalExceptionHandler.kt
@@ -2,8 +2,11 @@ package com.official.cufitapi.common.exception
 
 import com.official.cufitapi.common.config.ErrorCode
 import com.official.cufitapi.domain.notification.infrastructure.temp.DiscordWebhookClientAdapter
+import io.sentry.Sentry
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.lang.RuntimeException
+import kotlin.RuntimeException
 
 @RestControllerAdvice
 class GlobalExceptionHandler(
@@ -13,30 +16,42 @@ class GlobalExceptionHandler(
     @ExceptionHandler(BadRequestException::class)
     fun handleBadRequestException(e: BadRequestException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
+        Sentry.captureException(e)
         return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
     }
 
     @ExceptionHandler(UnAuthorizedException::class)
     fun handleUnAuthorizedException(e: UnAuthorizedException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
+        Sentry.captureException(e)
         return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
     }
 
     @ExceptionHandler(NotFoundException::class)
     fun handleNotFoundException(e: NotFoundException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
+        Sentry.captureException(e)
         return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
     }
 
     @ExceptionHandler(InternalServerErrorException::class)
     fun handleInternalServerErrorException(e: InternalServerErrorException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
+        Sentry.captureException(e)
+        return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(e: RuntimeException): ErrorResponse {
+        discordWebhookClientAdapter.sendErrorAlert(e)
+        Sentry.captureException(e)
         return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
     }
 
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
+        Sentry.captureException(e)
         return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
     }
 }
