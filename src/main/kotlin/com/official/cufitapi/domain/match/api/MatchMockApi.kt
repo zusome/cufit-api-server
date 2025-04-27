092003d70c92ab776acc23bea00b10f4366cc326
woozi <kwj1270@naver.com>
2025-04-27 16:22:14 +0900

    feat(kwj1270): 후보자 추천 목록 API 실 데이터로 변경한다.

--- src/main/kotlin/com/official/cufitapi/domain/match/api/MatchMockApi.kt
+++ src/main/kotlin/com/official/cufitapi/domain/match/api/MatchMockApi.kt
@@ -22,9 +22,9 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@Profile("!local")
-@ApiV1Controller
-@RestController
+// @Profile("!local")
+// @ApiV1Controller
+// @RestController
 class MatchMockApi(
     private val suggestMatchUseCase: SuggestMatchUsecase,
     private val updateMatchUseCase: UpdateMatchUsecase,
