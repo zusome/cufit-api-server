0718e89b8bbec0027e8a6f3300345989c4cf8966
woozi <kwj1270@naver.com>
2025-05-06 00:22:24 +0900

    feat(kwj1270): 에러 발생시 사전 정의된 코드를 내려준다.

--- src/main/kotlin/com/official/cufitapi/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/official/cufitapi/common/exception/GlobalExceptionHandler.kt
@@ -16,42 +16,42 @@ class GlobalExceptionHandler(
     fun handleBadRequestException(e: BadRequestException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
         Sentry.captureException(e)
-        return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
+        return ErrorResponse(e.getCode(), ErrorCode.valueOf(e.message.toString()).message)
     }
 
     @ExceptionHandler(UnAuthorizedException::class)
     fun handleUnAuthorizedException(e: UnAuthorizedException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
         Sentry.captureException(e)
-        return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
+        return ErrorResponse(e.getCode(), ErrorCode.valueOf(e.message.toString()).message)
     }
 
     @ExceptionHandler(NotFoundException::class)
     fun handleNotFoundException(e: NotFoundException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
         Sentry.captureException(e)
-        return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
+        return ErrorResponse(e.getCode(), ErrorCode.valueOf(e.message.toString()).message)
     }
 
     @ExceptionHandler(InternalServerErrorException::class)
     fun handleInternalServerErrorException(e: InternalServerErrorException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
         Sentry.captureException(e)
-        return ErrorResponse(e.message.toString(), ErrorCode.valueOf(e.message.toString()).message)
+        return ErrorResponse(e.getCode(), ErrorCode.valueOf(e.message.toString()).message)
     }
 
     @ExceptionHandler(RuntimeException::class)
     fun handleRuntimeException(e: RuntimeException): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
         Sentry.captureException(e)
-        return ErrorResponse(e.message.toString(), ErrorCode.RUNTIME_EXCEPTION.message)
+        return ErrorResponse(ErrorCode.RUNTIME_EXCEPTION.name, ErrorCode.RUNTIME_EXCEPTION.message)
     }
 
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ErrorResponse {
         discordWebhookClientAdapter.sendErrorAlert(e)
         Sentry.captureException(e)
-        return ErrorResponse(e.message.toString(), ErrorCode.INTERNAL_SERVER_ERROR.message)
+        return ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR.name, ErrorCode.INTERNAL_SERVER_ERROR.message)
     }
 }
 
