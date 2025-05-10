54123dd88e045ad48f56988f6b918accb35cf6ae
woozi <50267433+kwj1270@users.noreply.github.com>
2025-05-11 08:37:37 +0900

    feat(kwj1270): 인앱 알림 목록 조회 기능 추가 (#129)
    * feat(kwj1270): 내후보자 목록 조회시 id 추가 기입 (#126)
    
    * feat(kwj1270): 내후보자 목록 조회시 id 추가 기입
    
    * feat(kwj1270): 내후보자 목록 조회시 id 추가 기입
    
    * feat(kwj1270): 인앱 알림 목록 조회 기능 추가 (#128)
    
    * feat(kwj1270): 내후보자 목록 조회시 id 추가 기입
    
    * feat(kwj1270): 내후보자 목록 조회시 id 추가 기입
    
    * feat(kwj1270): 인앱 알림 목록 조회 기능 추가

--- src/main/kotlin/com/official/cufitapi/domain/notification/api/http/NotificationApi.kt
+++ src/main/kotlin/com/official/cufitapi/domain/notification/api/http/NotificationApi.kt
@@ -7,31 +7,31 @@ import com.official.cufitapi.common.api.ApiV1Controller
 import com.official.cufitapi.common.api.dto.HttpResponse
 import com.official.cufitapi.domain.notification.api.http.docs.NotificationApiDocs
 import com.official.cufitapi.domain.notification.api.http.dto.NotificationResponse
-import com.official.cufitapi.domain.notification.application.temp.NotificationFindUseCase
+import com.official.cufitapi.domain.notification.infrastructure.persistence.NotificationDao
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 
-// @ApiV1Controller
-// class NotificationApi(
-//     private val notificationFindUseCase: NotificationFindUseCase,
-// ) : NotificationApiDocs {
-//
-//     @GetMapping("/notifications")
-//     override fun getNotificationList(
-//         @Authorization(AuthorizationType.ALL) authorizationUser: AuthorizationUser
-//     ): HttpResponse<List<NotificationResponse>> {
-//         val notifications = notificationFindUseCase.findAll(authorizationUser.userId)
-//         return HttpResponse.of(
-//             HttpStatus.OK,
-//             notifications.map {
-//                 NotificationResponse(
-//                     id = it.id,
-//                     title = it.title,
-//                     content = it.content,
-//                     notificationType = it.notificationType,
-//                     createdDate = it.createdDate
-//                 )
-//             }
-//         )
-//     }
-// }
+@ApiV1Controller
+class NotificationApi(
+    private val notificationDao: NotificationDao,
+) : NotificationApiDocs {
+
+    @GetMapping("/notifications")
+    override fun getNotificationList(
+        @Authorization(AuthorizationType.ALL) authorizationUser: AuthorizationUser,
+    ): HttpResponse<List<NotificationResponse>> {
+        val notifications = notificationDao.findAll(authorizationUser.userId)
+        return HttpResponse.of(
+            HttpStatus.OK,
+            notifications.map {
+                NotificationResponse(
+                    id = it.id,
+                    title = it.title,
+                    content = it.content,
+                    notificationType = it.notificationType,
+                    createdDate = it.createdDate
+                )
+            }
+        )
+    }
+}
