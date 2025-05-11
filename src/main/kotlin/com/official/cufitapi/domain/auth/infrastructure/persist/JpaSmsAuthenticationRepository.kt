d1c74265ae038f87ec71ec26d486c0b4b5388626
woozi <kwj1270@naver.com>
2025-05-11 21:23:00 +0900

    feat(kwj1270): 마지막 SMS 인증 코드에 대한 검증을 진행한다.

--- src/main/kotlin/com/official/cufitapi/domain/auth/infrastructure/persist/JpaSmsAuthenticationRepository.kt
+++ src/main/kotlin/com/official/cufitapi/domain/auth/infrastructure/persist/JpaSmsAuthenticationRepository.kt
@@ -3,5 +3,4 @@ package com.official.cufitapi.domain.auth.infrastructure.persist
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface JpaSmsAuthenticationRepository : JpaRepository<JpaSmsAuthentication, Long> {
-    fun findAll(memberId: Long): List<JpaSmsAuthentication>
 }
